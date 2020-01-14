6ad4aac92eadf070401e0f4cca487d0792098a95
vkandoba <vkandoba@skbkontur.ru>
2020-01-14 11:29:28 +0500

    add tokens for 2- and 3- num degree and implement the greedy strategy of making one number

--- test_w2n.py
+++ test_w2n.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from w2n import make_num
+from w2n import make_num, make_num_greedy
 
 
 class TestMakeNumbers(TestCase):
@@ -10,6 +10,14 @@ class TestMakeNumbers(TestCase):
     def test_make_with_spaces(self):
         self.assertEqual([1, 2], make_num('   один    два   '))
 
-    def test_make_numbers(self):
+    def test_make_digits(self):
         self.assertEqual([9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
                          make_num('девять восемь семь шесть пять четыре три два один ноль'))
+
+    def test_make_greedy(self):
+        self.assertEqual(245, make_num_greedy(['двести', 'сорок', 'пять']))
+        self.assertEqual(205, make_num_greedy(['двести', 'пять']))
+
+    def test_make_greedy_with_another_num_at_end(self):
+        self.assertEqual(200, make_num_greedy(['двести', 'триста']))
+        self.assertEqual(200, make_num_greedy(['двести', 'ноль']))
--- w2n.py
+++ w2n.py
@@ -1,5 +1,5 @@
 tokens_config = {
-    'ноль': {'num': 0, 'level': 1},
+    'ноль': {'num': 0, 'level': 10},
     'один': {'num': 1, 'level': 1},
     'одна': {'num': 1, 'level': 1},
     'два': {'num': 2, 'level': 1},
@@ -12,9 +12,40 @@ tokens_config = {
     'восемь': {'num': 8, 'level': 1},
     'девять': {'num': 9, 'level': 1},
     "десять": {'num': 10, 'level': 1},
+    "двадцать": {'num': 20, 'level': 2},
+    "тридцать": {'num': 30, 'level': 2},
+    "сорок": {'num': 40, 'level': 2},
+    "пятьдесят": {'num': 50, 'level': 2},
+    "шестьдесят": {'num': 60, 'level': 2},
+    "семьдесят": {'num': 70, 'level': 2},
+    "восемьдесят": {'num': 80, 'level': 2},
+    "девяносто": {'num': 90, 'level': 2},
+    "сто": {'num': 100, 'level': 3},
+    "двести": {'num': 200, 'level': 3},
+    "триста": {'num': 300, 'level': 3},
+    "четыреста": {'num': 400, 'level': 3},
+    "пятьсот": {'num': 500, 'level': 3},
+    "шестьсот": {'num': 600, 'level': 3},
+    "семьсот": {'num': 700, 'level': 3},
+    "восемьсот": {'num': 800, 'level': 3},
+    "девятьсот": {'num': 900, 'level': 3}
 }
 
 
 def make_num(text):
     words = text.split()
     return [tokens_config[w]['num'] if w in tokens_config else w for w in words]
+
+
+def make_num_greedy(words):
+    return make_num_greedy_internal(words, tokens_config[words[0]]['level'])
+
+
+def make_num_greedy_internal(words, level):
+    if not words or words[0] not in tokens_config:
+        return 0;
+    num_token = tokens_config[words[0]]
+    if num_token['level'] > level:
+        return 0
+    else:
+        return num_token['num'] + make_num_greedy_internal(words[1:], level-1)
