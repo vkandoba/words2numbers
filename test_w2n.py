bc000637facb0f790b7d063d2bc19d7644158ed1
vkandoba <vkandoba@skbkontur.ru>
2020-01-14 10:34:50 +0500

    Add the tokens config with values and a level of word's digit. Rename the main function make_numbers -> make_num

--- test_w2n.py
+++ test_w2n.py
@@ -1,8 +1,15 @@
 from unittest import TestCase
 
-from w2n import make_numbers
+from w2n import make_num
 
 
 class TestMakeNumbers(TestCase):
+    def test_make_with_empty(self):
+        self.assertEqual([], make_num(''))
+
+    def test_make_with_spaces(self):
+        self.assertEqual([1, 2], make_num('   один    два   '))
+
     def test_make_numbers(self):
-        self.assertEqual([1, 2, 3], make_numbers('один два три'))
+        self.assertEqual([9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
+                         make_num('девять восемь семь шесть пять четыре три два один ноль'))
--- w2n.py
+++ w2n.py
@@ -1,10 +1,20 @@
-numeral_words = {
-    'один': 1,
-    'два': 2,
-    'три': 3
+tokens_config = {
+    'ноль': {'num': 0, 'level': 1},
+    'один': {'num': 1, 'level': 1},
+    'одна': {'num': 1, 'level': 1},
+    'два': {'num': 2, 'level': 1},
+    'две': {'num': 2, 'level': 1},
+    'три': {'num': 3, 'level': 1},
+    'четыре': {'num': 4, 'level': 1},
+    'пять': {'num': 5, 'level': 1},
+    'шесть': {'num': 6, 'level': 1},
+    'семь': {'num': 7, 'level': 1},
+    'восемь': {'num': 8, 'level': 1},
+    'девять': {'num': 9, 'level': 1},
+    "десять": {'num': 10, 'level': 1},
 }
 
 
-def make_numbers(str):
-    words = str.split()
-    return [numeral_words[w] if w in numeral_words else w for w in words]
+def make_num(text):
+    words = text.split()
+    return [tokens_config[w]['num'] if w in tokens_config else w for w in words]
